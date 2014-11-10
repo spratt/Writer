c42ce1925c274eadedbc12350b62343d4e12479c
Simon Pratt <simon@pr4tt.com>
2014-11-09 21:46:22 -0500

    Now saving to local storage

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
-tinymce.init({
-    selector: "textarea",
-    plugins: [
-        "advlist autolink lists link image charmap print preview anchor",
-        "searchreplace visualblocks code fullscreen save",
-        "insertdatetime media table contextmenu paste"
-    ],
-    toolbar: "save | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-    setup: function(editor) {
-		editor.on('init', function() {
-			setTimeout(function() {
-				tinyMCE.editors[0].execCommand('mceFullScreen');
-			},0);
-		});
-	},
-	save_enablewhendirty: true,
-    save_onsavecallback: function(editor) {
-		console.log("Save");
-		console.log(editor.save());
+$(function() {
+	var filename = "fix_me.html";
+	
+	function save(editor) {
+		if (Modernizr.localstorage) {
+			localStorage.setItem("file_" + filename, editor.save());
+		} else {
+			alert("Local storage is required to save.");
+		}
 	}
+
+	tinymce.init({
+		selector: "textarea",
+		plugins: [
+			"advlist autolink lists link image charmap print preview anchor",
+			"searchreplace visualblocks code fullscreen save",
+			"insertdatetime media table contextmenu paste"
+		],
+		toolbar: "save | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+		setup: function(editor) {
+			editor.on('init', function() {
+				setTimeout(function() {
+					tinyMCE.editors[0].execCommand('mceFullScreen');
+				},0);
+			});
+		},
+		save_enablewhendirty: true,
+		save_onsavecallback: save
+	});
 });
